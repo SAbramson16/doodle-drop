4d1495df5c86eb24189149a7d4c20a16ca2de6cd
Sharon Abramson <sharabramson@gmail.com>
2024-01-11 13:54:36 -0500

    modifying models and relationships

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// NOT WORKING YET
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -16,6 +17,8 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+// NOT WORKING YET
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -48,6 +51,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// NOT WORKING YET
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
