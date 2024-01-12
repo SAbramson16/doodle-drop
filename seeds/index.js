3afd1641a97fa3e89aa28b771cdc9574baadf7c4
Sharon Abramson <sharabramson@gmail.com>
2024-01-12 10:11:24 -0500

    fixed bug in userSeeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,16 @@
 const seedCategories = require('./categorySeeds');
 const seedArt = require('./artSeeds');
+const seedUsers = require('./userSeeds')
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
+
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
